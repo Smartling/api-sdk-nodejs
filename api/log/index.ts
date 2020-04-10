53c8017ac759caad302bc304879365a56b1ee65d
Loparev <pavel.loparev.brdm2@gmail.com>
2020-04-10 13:36:20 +0300

    Added log api support

---
+++ api/log/index.ts
@@ -0,0 +1,22 @@
+import SmartlingAuthApi from "../auth";
+import SmartlingBaseApi from "../base";
+import { CreateLogParameters } from "./params/create-log-parameters";
+
+export class SmartlingLogApi extends SmartlingBaseApi {
+    private readonly authApi: SmartlingAuthApi;
+    private readonly entrypoint: string;
+
+    constructor(authApi: SmartlingAuthApi, logger, smartlingApiBaseUrl: string) {
+        super(logger);
+        this.authApi = authApi;
+        this.entrypoint = `${smartlingApiBaseUrl}/updates`;
+    }
+
+    public async log(payload: CreateLogParameters): Promise<void> {
+        await this.makeRequest(
+            "post",
+            `${this.entrypoint}/status`,
+            JSON.stringify(payload.export())
+        );
+    }
+}
