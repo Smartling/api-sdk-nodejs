82b0d5bc694efafd7c73b5ff78fe0ca4d2657996
Loparev <pavel.loparev.brdm2@gmail.com>
2021-12-30 02:13:44 +0200

    Fixed eslint reported issues

--- test/stream-to-string.ts
+++ test/stream-to-string.ts
@@ -1,13 +1,16 @@
-function streamToString(stream) {
+import { Readable } from "stream";
+
+function streamToString(stream: Readable): Promise<string> {
     const chunks = [];
 
     stream.resume();
 
     return new Promise((resolve, reject) => {
-        stream.on("data", (chunk) => chunks.push(Buffer.from(chunk)));
-        stream.on("error", (err) => reject(err));
+        // eslint-disable-next-line fp/no-mutating-methods
+        stream.on("data", chunk => chunks.push(Buffer.from(chunk)));
+        stream.on("error", err => reject(err));
         stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
-    })
+    });
 }
 
 export { streamToString };
